9bdaa5f6f55ed1da9e0f47913abb2d70e16372e3
Dice <j.streicher@protonmail.com>
2023-02-15 09:43:10 +0100

    Added Skip List implementation Some superficial test methods have been provided. Some problem exist with the remove method. Connected to the probabilistic nature of the skiplist as the problem appears seemingly randomly.

--- diceapplication/src/main/java/diceapplication/DiceMain.java
+++ diceapplication/src/main/java/diceapplication/DiceMain.java
@@ -4,12 +4,6 @@ import java.util.*;
 
 public class DiceMain {
     public static void main(String[] args) {
-       int[] array = {1,2,3,4,5};
-
-       for(int i = 0, j = array.length-1; i!=j; i++,j--){
-           int iTemp = array[i];
-           array[i] = array[j];
-           array[j] = array[i];
-       }
+       System.out.println(8/2*(2+2));
     }
 }
--- dicedatastructures/src/test/java/DiceSkipListTest.java
+++ dicedatastructures/src/test/java/DiceSkipListTest.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class DiceSkipListTest {
+import datalists.DiceSkipList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import static org.assertj.core.api.Assertions.*;
+public class DiceSkipListTest {
+    DiceSkipList<Integer> skipList;
+    LinkedList<Integer> integerList = new LinkedList<>();
+
+    @BeforeEach
+    void setUp(){
+        skipList = new DiceSkipList<Integer>();
+        integerList.add(5);
+        integerList.add(null);
+        integerList.add(-50);
+        integerList.add(0);
+    }
+
+    @Test
+    void testAdd() {
+        skipList.add(integerList.get(0));
+        assertThat(skipList.contains(5)).isTrue();
+        assertThat(skipList.add(null)).isFalse();
+        skipList.add(10);
+        skipList.add(-100);
+        assertThat(skipList.contains(10)).isTrue();
+        assertThat(skipList.contains(199)).isFalse();
+        assertThat(skipList.contains(-100)).isTrue();
+    }
+
+    @Test
+    void testRemove(){
+        skipList.addAll(this.integerList);
+        skipList.remove(-50);
+        assertThat(skipList.contains(-50)).isFalse();
+    }
+
+
 }
